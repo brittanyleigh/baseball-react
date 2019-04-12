546a185026da7b54faa04b8bd53ab73c3705a931
Brittany Isenberg <bisenberg@oncell.com>
2019-04-11 22:39:21 -0400

    standings & stats styles

--- src/components/Standings.js
+++ src/components/Standings.js
@@ -2,17 +2,24 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class Standings extends React.Component {
+  formatAVG(average){
+    return average.slice(1);
+  }
+  
   renderTeams(){
     if (this.props.standings.teamentry) {
       return this.props.standings.teamentry.map((team) => {      
         return (
-          <div className={` ${this.props.parentClass}__row standings`} key={team.team.ID}>
-            <span className={`${this.props.parentClass}__span`}>{team.rank} </span>
-            <span className={`${this.props.parentClass}__span ${this.props.parentClass}__span--grow`}>{team.team.City} {team.team.Name} </span>
-            <span className={`${this.props.parentClass}__span standings--winslosses-${team.rank}`}>{team.stats.Wins["#text"]}-{team.stats.Losses["#text"]} </span>
-            <span className={`${this.props.parentClass}__span`}>%{team.stats.WinPct["#text"]} </span>
-            <span className={`${this.props.parentClass}__span`}>{team.stats.GamesBack["#text"]}</span>
-          </div>
+          <tr className={` ${this.props.parentClass}__tr standings`} key={team.team.ID}>
+            <td className={`${this.props.parentClass}__item`}>{team.rank} </td>
+            <td className={`${this.props.parentClass}__item ${this.props.parentClass}__item--grow`}>
+              <span className="standings__team-name">{team.team.City} {team.team.Name}</span> 
+              <span className="standings__team-abbreviation">{team.team.Abbreviation}</span> 
+            </td>
+            <td className={`${this.props.parentClass}__item standings--winslosses-${team.rank}`}>{team.stats.Wins["#text"]}-{team.stats.Losses["#text"]} </td>
+            <td className={`${this.props.parentClass}__item`}>{this.formatAVG(team.stats.WinPct["#text"])} </td>
+            <td className={`${this.props.parentClass}__item`}>{team.stats.GamesBack["#text"]}</td>
+          </tr>
         );
       });       
     } else {
@@ -22,14 +29,20 @@ class Standings extends React.Component {
   
   render() {
     return (
-      <React.Fragment>
-        <div className={`${this.props.parentClass}__row ${this.props.parentClass}__row-heading `}>
-          <span className={`${this.props.parentClass}__span`}></span>
-          <span className={`${this.props.parentClass}__span`}></span>
-          <span className={`${this.props.parentClass}__span`}></span>
-        </div>
-        {this.renderTeams()}   
-      </React.Fragment>  
+      <table className={`${this.props.parentClass}__table`}>
+        <thead>
+          <tr className={`${this.props.parentClass}__tr ${this.props.parentClass}__tr-heading `}>
+            <td></td>
+            <td></td>
+            <td className={`${this.props.parentClass}__tr-heading-item`}>W-L</td>
+            <td className={`${this.props.parentClass}__tr-heading-item`}>AVG</td>
+            <td className={`${this.props.parentClass}__tr-heading-item`}>GB</td>
+          </tr>
+        </thead>
+        <tbody>
+          {this.renderTeams()}  
+        </tbody>
+      </table>  
     )
 
 
