b78610e81560b7d80a5e47e97987ee0490a5d97d
Brittany Isenberg <brittany.isenberg@gmail.com>
2020-03-28 15:37:23 -0400

    convert Main from class to function

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,5 @@
-import React from "react";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 import YesterdayScore from "./YesterdayScore.js";
 import TodayGame from "./TodayGame.js";
@@ -16,124 +15,70 @@ import { getHitterStats } from "../ducks/hitterStats";
 import { getPitcherStats } from "../ducks/pitcherStats";
 import { getTeamNews } from "../ducks/news";
 
-const initialState = {
-  isOpen: false,
-  team: undefined
-};
+function Main() {
+  const store = useSelector(state => state);
+  const dispatch = useDispatch();
+  const { team, hitterStats, pitcherStats } = store;
+  const selected_team = team.team;
+  let hitterStatsContent, pitcherStatsContent;
 
-class Main extends React.Component {
-  state = initialState;
+  useEffect(() => {
+    dispatch(getYesterdayScore());
+    dispatch(getTodayGame());
+    dispatch(getDivisionStandings());
+    dispatch(getHitterStats());
+    dispatch(getPitcherStats());
+    dispatch(getTeamNews());
+  }, [selected_team]);
 
-  componentDidMount() {
-    this.updateAllStats();
-  }
-
-  componentDidUpdate(prevProps) {
-    if (this.props.selected_team !== prevProps.selected_team) {
-      this.updateAllStats();
-    }
-  }
-
-  updateAllStats() {
-    this.props.getYesterdayScore();
-    this.props.getTodayGame();
-    this.props.getDivisionStandings();
-    this.props.getHitterStats();
-    this.props.getPitcherStats();
-    this.props.getTeamNews();
-  }
-
-  renderHitterStats() {
-    const { hitterStats, selected_team } = this.props;
-
-    if (hitterStats.isFetching) {
-      return (
-        <PlaceholderBlock placeholderRows={3} team={selected_team.className} />
-      );
-    } else if (hitterStats && hitterStats.data) {
-      const statKeys = Object.keys(hitterStats.data);
-      return statKeys.map(stat => {
-        return (
-          <PlayerStats
-            key={stat}
-            stat={hitterStats.data[stat]}
-            className={selected_team.className}
-            statName={stat}
-          />
-        );
-      });
-    }
-    return null;
-  }
-
-  renderPitcherStats() {
-    const { pitcherStats, selected_team } = this.props;
-
-    if (pitcherStats.isFetching) {
-      return (
-        <PlaceholderBlock placeholderRows={3} team={selected_team.className} />
-      );
-    } else if (pitcherStats && pitcherStats.data) {
-      const statKeys = Object.keys(pitcherStats.data);
-      return statKeys.map(stat => {
-        return (
-          <PlayerStats
-            key={stat}
-            stat={pitcherStats.data[stat]}
-            className={selected_team.className}
-            statName={stat}
-          />
-        );
-      });
-    }
-    return null;
-  }
-
-  render() {
-    const { selected_team } = this.props;
-    return (
-      <main role="main" className={`main main--${selected_team.className}`}>
-        <div className="container">
-          <YesterdayScore />
-          <TodayGame />
-          <Standings />
-          {this.renderHitterStats()}
-          {this.renderPitcherStats()}
-          <TeamNews team={selected_team.className} />
-        </div>
-      </main>
+  if (hitterStats.isFetching) {
+    hitterStatsContent = (
+      <PlaceholderBlock placeholderRows={3} team={selected_team.className} />
     );
+  } else if (hitterStats && hitterStats.data) {
+    const statKeys = Object.keys(hitterStats.data);
+    hitterStatsContent = statKeys.map(stat => {
+      return (
+        <PlayerStats
+          key={stat}
+          stat={hitterStats.data[stat]}
+          className={selected_team.className}
+          statName={stat}
+        />
+      );
+    });
   }
+
+  if (pitcherStats.isFetching) {
+    pitcherStatsContent = (
+      <PlaceholderBlock placeholderRows={3} team={selected_team.className} />
+    );
+  } else if (pitcherStats && pitcherStats.data) {
+    const statKeys = Object.keys(pitcherStats.data);
+    pitcherStatsContent = statKeys.map(stat => {
+      return (
+        <PlayerStats
+          key={stat}
+          stat={pitcherStats.data[stat]}
+          className={selected_team.className}
+          statName={stat}
+        />
+      );
+    });
+  }
+
+  return (
+    <main role="main" className={`main main--${selected_team.className}`}>
+      <div className="container">
+        <YesterdayScore />
+        <TodayGame />
+        <Standings />
+        {hitterStatsContent}
+        {pitcherStatsContent}
+        <TeamNews team={selected_team.className} />
+      </div>
+    </main>
+  );
 }
 
-Main.propTypes = {
-  selected_team: PropTypes.object.isRequired,
-  hitterStats: PropTypes.object,
-  pitcherStats: PropTypes.object,
-  getYesterdayScore: PropTypes.func.isRequired,
-  getTodayGame: PropTypes.func.isRequired,
-  getDivisionStandings: PropTypes.func.isRequired,
-  getHitterStats: PropTypes.func.isRequired,
-  getPitcherStats: PropTypes.func.isRequired,
-  getTeamNews: PropTypes.func.isRequired
-};
-
-const mapStateToProps = state => {
-  return {
-    selected_team: state.team.team,
-    hitterStats: state.hitterStats,
-    pitcherStats: state.pitcherStats
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  {
-    getYesterdayScore,
-    getTodayGame,
-    getDivisionStandings,
-    getHitterStats,
-    getPitcherStats,
-    getTeamNews
-  }
-)(Main);
+export default Main;
